35cb9d0b00713f51fb2ba60337fd4ad149d4346e
Sebastian Alfaro <sebastian.alfaro@ultra.me>
2024-06-11 18:00:12 -0300

    chore: Adding snippets for JS, JSX, TS and TSX

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -10,40 +10,40 @@ return {
         pyright = {},
         html = {},
         tsserver = {
-					root_dir = function(...)
-						return require("lspconfig.util").root_pattern(".git")(...)
-					end,
-					single_file_support = true,
+          root_dir = function(...)
+            return require("lspconfig.util").root_pattern(".git")(...)
+          end,
+          single_file_support = true,
           filetypes = {
             "typescript",
             "typescriptreact",
             "javascript",
             "javascriptreact",
             "javascript.jsx",
-            "typescript.tsx"
+            "typescript.tsx",
           },
           settings = {
             javascript = {
-              inlayHints = {
-                includeInlayEnumMemberValueHints = true,
-                includeInlayFunctionLikeReturnTypeHints = true,
-                includeInlayFunctionParameterTypeHints = true,
-                includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
-                includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-                includeInlayPropertyDeclarationTypeHints = true,
-                includeInlayVariableTypeHints = true,
-              },
+              -- inlayHints = {
+              --   includeInlayEnumMemberValueHints = true,
+              --   includeInlayFunctionLikeReturnTypeHints = true,
+              --   includeInlayFunctionParameterTypeHints = true,
+              --   includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
+              --   includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+              --   includeInlayPropertyDeclarationTypeHints = true,
+              --   includeInlayVariableTypeHints = true,
+              -- },
             },
             typescript = {
-              inlayHints = {
-                includeInlayEnumMemberValueHints = true,
-                includeInlayFunctionLikeReturnTypeHints = true,
-                includeInlayFunctionParameterTypeHints = true,
-                includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
-                includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-                includeInlayPropertyDeclarationTypeHints = true,
-                includeInlayVariableTypeHints = true,
-              },
+              -- inlayHints = {
+              --   includeInlayEnumMemberValueHints = true,
+              --   includeInlayFunctionLikeReturnTypeHints = true,
+              --   includeInlayFunctionParameterTypeHints = true,
+              --   includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
+              --   includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+              --   includeInlayPropertyDeclarationTypeHints = true,
+              --   includeInlayVariableTypeHints = true,
+              -- },
             },
           },
         },
@@ -71,89 +71,31 @@ return {
         --     "markdown",
         --   },
         -- },
-        emmet_ls = {
+        -- emmet_ls = {
+        --   filetypes = {
+        --     "astro",
+        --     "blade",
+        --     -- "css",
+        --     "eruby",
+        --     "html",
+        --     "htmldjango",
+        --     "javascriptreact",
+        --     "less",
+        --     "pug",
+        --     -- "sass",
+        --     -- "scss",
+        --     "svelte",
+        --     "typescriptreact",
+        --     "vue",
+        --     "php",
+        --   },
+        -- },
+        intelephense = {
           filetypes = {
-            "astro",
-            "blade",
-            -- "css",
-            "eruby",
-            "html",
-            "htmldjango",
-            "javascriptreact",
-            "less",
-            "pug",
-            -- "sass",
-            -- "scss",
-            "svelte",
-            "typescriptreact",
-            "vue",
             "php",
           },
         },
-        intelephense = {
-          filetypes = {
-            "php"
-          }
-        },
       },
     },
-  },
-
-  -- add tsserver and setup with typescript.nvim instead of lspconfig
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "jose-elias-alvarez/typescript.nvim",
-      init = function()
-        require("lazyvim.util").lsp.on_attach(function(_, buffer)
-          -- stylua: ignore
-          vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
-          vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
-        end)
-      end,
-    },
-    ---@class PluginLspOpts
-    opts = {
-      ---@type lspconfig.options
-      servers = {
-        -- tsserver will be automatically installed with mason and loaded with lspconfig
-        tsserver = {},
-      },
-      -- you can do any additional lsp server setup here
-      -- return true if you don't want this server to be setup with lspconfig
-      ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
-      setup = {
-        -- example to setup with typescript.nvim
-        tsserver = function(_, opts)
-          require("typescript").setup({ server = opts })
-          return true
-        end,
-        -- Specify * to use this function as a fallback for any server
-        -- ["*"] = function(server, opts) end,
-      },
-    },
-  },
-
-  -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
-  -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
-  { import = "lazyvim.plugins.extras.lang.typescript" },
-  {
-    -- "neovim/nvim-lspconfig",
-    -- opts = function()
-    --   require("lspconfig").tsserver.setup({
-    --     init_options = {
-    --       preferences = {
-    --         disableSuggestions = true,
-    --       },
-    --     },
-    --     filetypes = {
-    --       "javascript",
-    --       "typescript",
-    --       "javascript",
-    --       "javascriptreact",
-    --       "php",
-    --     },
-    --   })
-    -- end,
   },
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,9 +1,21 @@
 -- Adding noice.nvim to telescope
 return {
-  "nvim-telescope/telescope.nvim",
-  opts = function()
-    LazyVim.on_load("telescope.nvim", function()
-      require("telescope").load_extension("noice")
-    end)
-  end,
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = function()
+      LazyVim.on_load("telescope.nvim", function()
+        require("telescope").load_extension("noice")
+      end)
+    end,
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      pickers = {
+        colorscheme = {
+          enable_preview = true,
+        },
+      },
+    },
+  },
 }
