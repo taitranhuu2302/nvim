1e261b5e663eed008c76c6cabde263b5c409cfdf
Sebastian Alfaro M <sebastianealfaro@gmail.com>
2024-07-07 23:46:44 -0300

    chore: commenting image.nvim

--- lua/plugins/images.lua
+++ lua/plugins/images.lua
@@ -1,76 +1,86 @@
--- Image support
--- ImageMagick: https://github.com/ImageMagick/ImageMagick
--- Magick LuaRock: https://github.com/leafo/magick
+-- This plugin requires `luarocks`. Try one of the following:
+--  - fix your `luarocks` installation
+--  - enable `hererocks` with `opts.rocks.hererocks = true`
+--  - disable `luarocks` support completely with `opts.rocks.enabled = false`
 return {
-  "3rd/image.nvim",
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-  },
-  config = function()
-    -- Example for configuring Neovim to load user-installed installed Lua rocks:
-    package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?/init.lua;"
-    package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?.lua;"
-
-    require("image").setup({
-      backend = "kitty",
-      kitty_method = "normal",
-      integrations = {
-        -- Notice these are the settings for markdown files
-        markdown = {
-          enabled = true,
-          clear_in_insert_mode = false,
-          -- Set this to false if you don't want to render images coming from
-          -- a URL
-          download_remote_images = true,
-          -- Change this if you would only like to render the image where the
-          -- cursor is at
-          -- I set this to true, because if the file has way too many images
-          -- it will be laggy and will take time for the initial load
-          only_render_image_at_cursor = true,
-          -- markdown extensions (ie. quarto) can go here
-          filetypes = { "markdown", "vimwiki" },
-        },
-        neorg = {
-          enabled = true,
-          clear_in_insert_mode = false,
-          download_remote_images = true,
-          only_render_image_at_cursor = false,
-          filetypes = { "norg" },
-        },
-        -- This is disabled by default
-        -- Detect and render images referenced in HTML files
-        -- Make sure you have an html treesitter parser installed
-        -- ~/github/dotfiles-latest/neovim/nvim-lazyvim/lua/plugins/treesitter.lua
-        -- html = {
-        --   enabled = true,
-        -- },
-
-        -- This is disabled by default
-        -- Detect and render images referenced in CSS files
-        -- Make sure you have a css treesitter parser installed
-        -- ~/github/dotfiles-latest/neovim/nvim-lazyvim/lua/plugins/treesitter.lua
-        -- css = {
-        --   enabled = true,
-        -- },
-      },
-      max_height = 12,
-      max_width = 128,
-      max_height_window_percentage = math.huge,
-      max_width_window_percentage = math.huge,
-
-      -- toggles images when windows are overlapped
-      window_overlap_clear_enabled = false,
-      window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
-
-      -- auto show/hide images when the editor gains/looses focus
-      editor_only_render_when_focused = false,
-
-      -- auto show/hide images in the correct tmux window
-      -- In the tmux.conf add `set -g visual-activity off`
-      tmux_show_only_in_active_window = true,
-
-      -- render image files as images when opened
-      hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" },
-    })
-  end,
+  -- "3rd/image.nvim",
+  -- dependencies = {
+  --   "leafo/magick",
+  --   "nvim-treesitter/nvim-treesitter",
+  --   {
+  --     "vhyrro/luarocks.nvim",
+  --     opts = {
+  --       rocks = {
+  --         hererocks = true,
+  --       },
+  --     },
+  --   },
+  -- },
+  -- config = function()
+  --   -- Example for configuring Neovim to load user-installed installed Lua rocks:
+  --   package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?/init.lua;"
+  --   package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?.lua;"
+  --
+  --   require("image").setup({
+  --     backend = "kitty",
+  --     kitty_method = "normal",
+  --     integrations = {
+  --       -- Notice these are the settings for markdown files
+  --       markdown = {
+  --         enabled = true,
+  --         clear_in_insert_mode = false,
+  --         -- Set this to false if you don't want to render images coming from
+  --         -- a URL
+  --         download_remote_images = true,
+  --         -- Change this if you would only like to render the image where the
+  --         -- cursor is at
+  --         -- I set this to true, because if the file has way too many images
+  --         -- it will be laggy and will take time for the initial load
+  --         only_render_image_at_cursor = true,
+  --         -- markdown extensions (ie. quarto) can go here
+  --         filetypes = { "markdown", "vimwiki" },
+  --       },
+  --       neorg = {
+  --         enabled = true,
+  --         clear_in_insert_mode = false,
+  --         download_remote_images = true,
+  --         only_render_image_at_cursor = false,
+  --         filetypes = { "norg" },
+  --       },
+  --       -- This is disabled by default
+  --       -- Detect and render images referenced in HTML files
+  --       -- Make sure you have an html treesitter parser installed
+  --       -- ~/github/dotfiles-latest/neovim/nvim-lazyvim/lua/plugins/treesitter.lua
+  --       -- html = {
+  --       --   enabled = true,
+  --       -- },
+  --
+  --       -- This is disabled by default
+  --       -- Detect and render images referenced in CSS files
+  --       -- Make sure you have a css treesitter parser installed
+  --       -- ~/github/dotfiles-latest/neovim/nvim-lazyvim/lua/plugins/treesitter.lua
+  --       -- css = {
+  --       --   enabled = true,
+  --       -- },
+  --     },
+  --     max_height = 12,
+  --     max_width = 128,
+  --     max_height_window_percentage = math.huge,
+  --     max_width_window_percentage = math.huge,
+  --
+  --     -- toggles images when windows are overlapped
+  --     window_overlap_clear_enabled = false,
+  --     window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+  --
+  --     -- auto show/hide images when the editor gains/looses focus
+  --     editor_only_render_when_focused = false,
+  --
+  --     -- auto show/hide images in the correct tmux window
+  --     -- In the tmux.conf add `set -g visual-activity off`
+  --     tmux_show_only_in_active_window = true,
+  --
+  --     -- render image files as images when opened
+  --     hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" },
+  --   })
+  -- end,
 }
