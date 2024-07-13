b60ee309a63e820f1038a08414b4c625f18f95bf
Sebastian Alfaro M <sebastianealfaro@gmail.com>
2024-07-13 09:27:41 -0300

    chore: adding autotag

--- lua/plugins/autoclose.lua
+++ lua/plugins/autoclose.lua
@@ -1,33 +1,56 @@
 return {
-  "echasnovski/mini.pairs",
-  event = "VeryLazy",
-  opts = {
-    modes = { insert = true, command = true, terminal = false },
-    -- skip autopair when next character is one of these
-    skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
-    -- skip autopair when the cursor is inside these treesitter nodes
-    skip_ts = { "string" },
-    -- skip autopair when next character is closing pair
-    -- and there are more closing pairs than opening pairs
-    skip_unbalanced = true,
-    -- better deal with markdown code blocks
-    markdown = true,
-  },
-  keys = {
-    {
-      "<leader>up",
-      function()
-        vim.g.minipairs_disable = not vim.g.minipairs_disable
-        if vim.g.minipairs_disable then
-          LazyVim.warn("Disabled auto pairs", { title = "Option" })
-        else
-          LazyVim.info("Enabled auto pairs", { title = "Option" })
-        end
-      end,
-      desc = "Toggle Auto Pairs",
+  {
+    "echasnovski/mini.pairs",
+    event = "VeryLazy",
+    opts = {
+      modes = { insert = true, command = true, terminal = false },
+      -- skip autopair when next character is one of these
+      skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+      -- skip autopair when the cursor is inside these treesitter nodes
+      skip_ts = { "string" },
+      -- skip autopair when next character is closing pair
+      -- and there are more closing pairs than opening pairs
+      skip_unbalanced = true,
+      -- better deal with markdown code blocks
+      markdown = true,
     },
+    keys = {
+      {
+        "<leader>up",
+        function()
+          vim.g.minipairs_disable = not vim.g.minipairs_disable
+          if vim.g.minipairs_disable then
+            LazyVim.warn("Disabled auto pairs", { title = "Option" })
+          else
+            LazyVim.info("Enabled auto pairs", { title = "Option" })
+          end
+        end,
+        desc = "Toggle Auto Pairs",
+      },
+    },
+    config = function(_, opts)
+      LazyVim.mini.pairs(opts)
+    end,
   },
-  config = function(_, opts)
-    LazyVim.mini.pairs(opts)
-  end,
+  {
+    "windwp/nvim-ts-autotag",
+    opts = function()
+      require("nvim-ts-autotag").setup({
+        opts = {
+          -- Defaults
+          enable_close = true, -- Auto close tags
+          enable_rename = true, -- Auto rename pairs of tags
+          enable_close_on_slash = false, -- Auto close on trailing </
+        },
+        -- Also override individual filetype configs, these take priority.
+        -- Empty by default, useful if one of the "opts" global settings
+        -- doesn't work well in a specific filetype
+        per_filetype = {
+          -- ["html"] = {
+          --   enable_close = false,
+          -- },
+        },
+      })
+    end,
+  },
 }
